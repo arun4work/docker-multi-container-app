20fb4f68cc9a2ecbf4f0a62d6b1eebdcd730f74b
Arun Bahal <a24ren@gmail.com>
2026-03-28 20:55:03 +0530

    Production deployment changes

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -9,26 +9,3 @@ describe('App', () => {
     expect(screen.getByText('Docker Multi Container App')).toBeInTheDocument();
   });
 });
-
-// describe('App', () => {
-//   it('renders the demo Vite text', () => {
-//     render(<App />);
-
-//     expect(screen.getByText('Vite + React')).toBeInTheDocument();
-//   });
-
-//   it('should increment count by 1 on click of count button', async () => {
-//     render(<App />);
-//     const user = userEvent.setup();
-
-//     //verify that initial count is 0
-//     const buttonWithText = screen.getByRole('count-button');
-//     expect(buttonWithText.textContent).toBe('count is 0');
-
-//     // Click the button
-//     await user.click(buttonWithText);
-
-//     // Assert that the count text is updated to 1
-//     expect(buttonWithText.textContent).toBe('count is 1');
-//   });
-// });
