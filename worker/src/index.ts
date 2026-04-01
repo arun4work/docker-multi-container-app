af0d12fe5e7fad926b1966cf1a8506f185ae0dbf
Arun Bahal <a24ren@gmail.com>
2026-04-02 00:51:42 +0530

    To deploy worker as web service to Render cloud

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,6 +1,23 @@
 import { keys } from './keys.js';
 import redis from 'redis';
 
+/**** To deploy worker as webservice to Render, it needs to have a port assigned to it.
+ * Creating a fake server with port for that.
+ * We can deploy as background workder in Render but that's paid service.
+ * */
+// This satisfies Render's Port Scan
+import http from 'http';
+const PORT = process.env.PORT || 10000;
+http
+  .createServer((req: any, res: any) => {
+    res.writeHead(200);
+    res.end('Worker Is Alive');
+  })
+  .listen(PORT);
+
+console.log(`Worker heartbeat listening on ${PORT}`);
+/** End of the fake server and port creation block */
+
 const redisUrl =
   process.env.REDIS_URL || `redis://${keys.redisHost}:${keys.redisPort}`;
 const isTls = redisUrl.startsWith('rediss://') as any;
